d67e7b80683c72955a66d11756f2c836dae9164a
theo_B <theobiancalana@gmail.com>
2019-02-07 15:13:41 +0100

    js

---
+++ js/Inscription.js
@@ -0,0 +1,8 @@
+window.addEventListener("load",()=>{
+
+    let mdp1 = document.querySelector("#inputPassword");
+    let mdp2 = document.querySelector("#verifinputPassword");
+
+    
+
+});
--- js/rechercheOffre.js
+++ js/rechercheOffre.js
@@ -3,5 +3,13 @@
 
 
 window.addEventListener("load",()=>{
+    let quoi = document.querySelector("#metier");
+    let ou = document.querySelector("#ville");
+    let brecherche =  document.querySelector("#boutonRecherche");
 
+    brecherche.addEventListener("click",lancerRecherche);
 });
+
+function lancerRecherche(event){
+    
+}
