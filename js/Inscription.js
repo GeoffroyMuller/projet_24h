9a1ae1318acde0a848b42ae9501181c4073cfa16
theo_B <theobiancalana@gmail.com>
2019-02-07 17:05:48 +0100

    js inscription

--- js/Inscription.js
+++ js/Inscription.js
@@ -7,14 +7,17 @@ window.addEventListener("load",()=>{
     let handinon = document.querySelector("#radioNon");
 
     handinon.addEventListener("click",masquerchamp);
-    handioui.addEventListener("click",afficherchamp)
+    handioui.addEventListener("click",afficherchamp);
+
+    mdp1.addEventListener("keyup",verifmdp);
+    mdp2.addEventListener("keyup",verifmdp);
 
     selectnon();
 
 });
 
 function selectnon(){
-    let bnon = document.querySelector("#radioNon");;
+    let bnon = document.querySelector("#radioNon");
     bnon.click();
 }
 
@@ -29,6 +32,20 @@ function masquerchamp(event) {
     $(champ).hide();
 }
 
-function submmitInscr(event) {
-    
+function verifmdp(event) {
+    console.log("keytype");
+    let mdp1 = document.querySelector("#inputPassword");
+    let mdp2 = document.querySelector("#verifinputPassword");
+
+    let inscrir = document.querySelector("#submit");
+    console.log(inscrir);
+
+
+    if(mdp1.value===mdp2.value){
+        inscrir.disabled=false;
+    }
+    else{
+
+        inscrir.disabled=true;
+    }
 }
