145f66fac5ad063b077353d3403ced9341246f08
theo_B <theobiancalana@gmail.com>
2019-02-07 15:58:52 +0100

    affichage option handicap

--- js/Inscription.js
+++ js/Inscription.js
@@ -3,6 +3,32 @@ window.addEventListener("load",()=>{
     let mdp1 = document.querySelector("#inputPassword");
     let mdp2 = document.querySelector("#verifinputPassword");
 
+    let handioui = document.querySelector("#radioOui");
+    let handinon = document.querySelector("#radioNon");
 
+    handinon.addEventListener("click",masquerchamp);
+    handioui.addEventListener("click",afficherchamp)
+
+    selectnon();
 
 });
+
+function selectnon(){
+    let bnon = document.querySelector("#radioNon");;
+    bnon.click();
+}
+
+function afficherchamp(event) {
+    let champ =document.querySelector("#descriphandicap");
+    $(champ).show();
+}
+
+
+function masquerchamp(event) {
+    let champ =document.querySelector("#descriphandicap");
+    $(champ).hide();
+}
+
+function submmitInscr(event) {
+    
+}
